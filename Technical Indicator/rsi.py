f4a14b4a34129be3aeed703c18b8f70723043e59
Vineet Dusa <100671151+Deevin07@users.noreply.github.com>
2022-05-28 20:42:47 +0530

    Add files via upload

---
+++ Technical Indicator/rsi.py
@@ -0,0 +1,37 @@
+# =============================================================================
+# Import OHLCV data and calculate RSI technical indicators
+# Author : Mayank Rasu (http://rasuquant.com/wp/)
+
+# Please report bug/issues in the Q&A section
+# =============================================================================
+
+# Import necesary libraries
+import yfinance as yf
+import numpy as np
+
+# Download historical data for required stocks
+tickers = ["AMZN","GOOG","MSFT"]                  # add stocks that you want
+ohlcv_data = {}
+
+# looping over tickers and storing OHLCV dataframe in dictionary
+for ticker in tickers:
+    temp = yf.download(ticker,period='1mo',interval='5m')
+    temp.dropna(how="any",inplace=True)
+    ohlcv_data[ticker] = temp
+    
+def RSI(DF, n=14):
+    "function to calculate RSI"
+    df = DF.copy()
+    df["change"] = df["Adj Close"] - df["Adj Close"].shift(1)
+    df["gain"] = np.where(df["change"]>=0, df["change"], 0)
+    df["loss"] = np.where(df["change"]<0, -1*df["change"], 0)
+    df["avgGain"] = df["gain"].ewm(alpha=1/n, min_periods=n).mean()
+    df["avgLoss"] = df["loss"].ewm(alpha=1/n, min_periods=n).mean()
+    df["rs"] = df["avgGain"]/df["avgLoss"]
+    df["rsi"] = 100 - (100/ (1 + df["rs"]))
+    return df["rsi"]
+
+
+for ticker in ohlcv_data:
+    ohlcv_data[ticker]["RSI"] = RSI(ohlcv_data[ticker])
+    
