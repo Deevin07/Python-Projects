b6a85a548288f549cf320abcd7bab5c380b2777b
Vineet Dusa <100671151+Deevin07@users.noreply.github.com>
2022-06-21 20:02:23 +0530

    Update coveration and correlation.py

--- Finance/coveration and correlation.py
+++ Finance/coveration and correlation.py
@@ -12,7 +12,7 @@ import pandas_datareader.data as web
 
 
 
-tickers  = ['PG','BEI.DE']          # the tickers you want 
+tickers  = ['RELIANCE.NS','^NSEI']         # the tickers you want 
 sec_data = pd.DataFrame()
 
 for t in tickers:
@@ -24,7 +24,9 @@ sec_data.tail()                                       #checking the data
 sec_return  = np.log(sec_data / sec_data.shift(1))
 sec_return
 
-sec_return[['PG','BEI.DE']].mean() * 250 * 0.5    # change the tickers you want data from 
+sec_return.describe()                               # getting a gist of the data we have 
+
+sec_return[['RELIANCE.NS','^NSEI']].mean() * 250 * 0.5    # change the tickers you want data from 
 
 
 #calculating covariance and correlation
