c98689a042db9b16ef8e8721384ccc4affd9d3aa
Vineet Dusa <100671151+Deevin07@users.noreply.github.com>
2022-11-02 13:52:04 +0530

    Add files via upload

---
+++ Technical Indicator/technical indicator from twelve data.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  2 13:38:32 2022
+
+@author: Vineet
+"""
+
+
+import requests
+import pandas as pd
+
+api_key = '190ad362175b43638975eac8e2dcad84'
+symbol = 'USD/JPY'
+interval = '1min'
+technicals = 'bbands'
+
+api_url = f'https://api.twelvedata.com/{technicals}?symbol={symbol}&start_date=2019-08-09&end_date=2022-01-01&interval={interval}&apikey={api_key}'
+
+data2 = requests.get(api_url).json()
+
+data2 = pd.DataFrame(data2['values'])
+
