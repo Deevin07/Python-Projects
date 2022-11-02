f1e593837d5f75d47cd3f086419efd7ac80da582
Vineet Dusa <100671151+Deevin07@users.noreply.github.com>
2022-11-02 16:38:42 +0530

    Add files via upload

---
+++ Finance/calculating the daily drawdown and max drawdown .py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  2 16:15:29 2022
+
+@author: Vineet
+"""
+
+import pandas as pd
+import requests
+import numpy as np
+import datetime as dt
+import warnings
+import matplotlib.pyplot as plt
+
+
+warnings.filterwarnings('ignore')
+
+api_key = '190ad362175b43638975eac8e2dcad84'
+symbol = 'BTC/USD'
+interval = '5min'
+order = 'asc'
+end_date = dt.datetime(2021,7,14)
+start_date = end_date - dt.timedelta(days=15)
+
+api_url = f'https://api.twelvedata.com/time_series?symbol={symbol}&start_date={start_date}&end_date={end_date}&interval={interval}&order={order}&apikey={api_key}'
+
+data = requests.get(api_url).json()
+print(data)
+
+data_final = pd.DataFrame(data['values'])
+data_final['close'] = pd.to_numeric(data_final['close'],errors='coerce')
+data_final.set_index('datetime',inplace = True)
+
+window = 24*12
+
+roll_max = data_final['close'].rolling(window, min_periods = 1).max() 
+dd = data_final['close'] / roll_max - 1
+
+max_draw = dd.rolling(window,min_periods = 1).min()
+
+
+"""
+blue is the daily draw down
+and the yellow is the max drawdown
+
+""" 
+dd.plot()
+max_draw.plot()
+plt.show()
